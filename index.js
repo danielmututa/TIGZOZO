ba01a5f7a7a229d21658a83dd20fee6711c9bf44
unknown <mututadaniel54@gmail.com>
2024-05-07 03:02:18 -0700

    index

---
+++ index.js
@@ -0,0 +1,19 @@
+// menu
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+//  end menu
+
+
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+
+ }
+ 
+ 
