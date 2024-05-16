e1bd61141884473d9d28748793b05ae6138f6412
unknown <mututadaniel54@gmail.com>
2024-05-16 05:17:58 -0700

    commit

--- index.js
+++ index.js
@@ -18,16 +18,6 @@ function myFunction() {
 
 
 
- function func(){
-  var email= document.getElementById("username").value;
-  var pass = document.getElementById("password").value;
-  if (email == "tigzozo@gmail.com" && pass =="tig@2024") {
-    alert("successfull")
-    window.location.assign("index.html")
-  }else{
-    alert("wrong entry invalid-input please try again")
-  }
- }
  
  
  
